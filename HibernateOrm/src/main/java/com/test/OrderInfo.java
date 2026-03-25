e81c7484f38b4e8a1c0ddf448df8d182fb783282
CHARAN J <gcharanroyal@gmail.com>
2026-03-25 13:11:37 +0530

    Add files via upload

---
+++ HibernateOrm/src/main/java/com/test/OrderInfo.java
@@ -0,0 +1,21 @@
+package com.test;
+
+import java.util.List;
+
+public class OrderInfo {
+public static void main(String[] args) {
+	OrderDao odao=new OrderDao();
+	Order obj=new Order();
+	obj.setId(2);
+	obj.setO_name("Electronics");
+	obj.setO_quantity(3);
+	obj.setO_price( 4567);
+	odao.saveOrder(obj);
+	//odao.updateOrder(obj);
+	//odao.deleteOrder(obj);
+	List<Order> list=odao.listAllOrders();
+	list.forEach(dt -> System.out.println(dt.getId()+" "+dt.getO_name()+" "+dt.getO_quantity()+" "+dt.getO_price()+" "));
+	
+	System.out.println("Done.");
+}
+}
